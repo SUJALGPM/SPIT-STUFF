640be9e51d794d70a48db067da6c2ac3c5fe3976
Sujal <mailme.sujaldingankar@gmail.com>
2024-11-19 23:52:22 +0530

    new5539

--- DSA/Practical_List/Ques24.c
+++ DSA/Practical_List/Ques24.c
@@ -1,9 +1,8 @@
 // 24. Add 2 non-negative numbers that have been given as a linked list Given two non-empty linked lists representing two non-negative integers. 
 //     The most significant digit comes first and each of their nodes contains a single digit. 
 //     Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself. 
-//     Example 1: operand_1: 7 → 2 → 3 → 3
-//     result: 
-//     Example 2: operand_2: 5 → 7 → 4 
+//     Example 1: operand_1: 7 → 2 → 3 → 3 
+//                operand_2: 5 → 7 → 4 
 //     result: 7 → 8 → 0 → 7
 
 
